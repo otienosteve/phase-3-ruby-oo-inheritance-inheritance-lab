a585bd3356771ea7b65d4e77b3bef1cafda78c25
otienosteve <steveotieno701@gmail.com>
2022-07-19 21:10:05 +0300

    commit tree 976154fd79316892fa61e7adab78073ce5c009dc

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
 
+require_relative './user.rb'
+
+class Student < User
+def initialize()
+    @knowledge=[]
+   
+    
+end
+
+def learn(knw)
+    @knowledge << knw
+
+end
+def knowledge
+    @knowledge
+end
 end
